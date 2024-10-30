eaebabc640c8c86bfc1bfa847698c2b1b81b78a9
alisson0liveir2 <145785362+alisson0liveir2@users.noreply.github.com>
2024-10-30 09:26:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Programção',
+    'O que é Python?',
+    'É uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'É um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz "Oi" em inglês?',
+    'Hi / Hello'
+)
+
